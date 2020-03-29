de767db48b334838ee2fc19654eb9848085754fc
Victor <vypl.snf19@uea.edu.br>
2020-03-29 11:38:04 -0400

    Logout

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 import api from '../../services/api';
 
@@ -12,6 +12,8 @@ function Profile() {
   const ongName = localStorage.getItem('ongName');
 
   const [incidents, setIncidents] = useState([]);
+
+  const history = useHistory();
 
   useEffect(() => {
     async function loadIncidents() {
@@ -58,6 +60,11 @@ function Profile() {
     }
   }
 
+  function handleLogout() {
+    localStorage.clear();
+    history.push('/');
+  }
+
   return (
     <div className="profile-container">
       <header>
@@ -65,7 +72,7 @@ function Profile() {
         <span>Bem vinda, {ongName}</span>
         
         <Link className="button" to="/incidents/new">Cadastrar Novo Caso</Link>
-        <button type="button">
+        <button type="button" onClick={handleLogout}>
           <FiPower size={18} color="#E02041"/>
         </button>
       </header>
